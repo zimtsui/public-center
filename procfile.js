052bc463d9e1f026748b244bafd4d21569ed02a5
Zim <zimtsui@gmail.com>
2019-10-03 13:49:11 +0800

    dev

--- procfile.js
+++ procfile.js
@@ -1,9 +1,16 @@
 'use strict';
 
 module.exports = (pandora) => {
+    pandora
+        .process('process1')
+        .scale(1)
+        .env({
+            NODE_ENV: pandora.env ? 'development' : 'production',
+        });
 
     pandora
-        .service('quote-center', './');
+        .service('quote-center', './')
+        .process('process1');
 
     /**
      * you can also use cluster mode to start application
